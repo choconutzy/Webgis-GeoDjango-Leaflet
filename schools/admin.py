688cb42cc0b03637d9cf24ec54cee56480ec1a2b
Aprilia Khoirunnisa <aprilianissa112@gmail.com>
2022-03-27 08:15:18 +0700

    chore:setup django, add school model

---
+++ schools/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
+from .models import School
+# Register your models here.
+class SchoolAdmin(LeafletGeoAdmin):
+    list_display=['name','province', 'district', 'level', 'male', 'female']
+
+admin.site.register(School, SchoolAdmin)
